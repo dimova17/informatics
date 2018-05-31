44d0e3f1d7d986f356ed6a47b7e3a5aac7bbd794
Alina <dimova_alina@mail.ru>
2018-05-31 08:57:02 +0300

    Исправления

--- delete.php
+++ delete.php
@@ -4,15 +4,25 @@ require_once 'connection.php';
 $link = mysqli_connect($host, $user, $password, $database)
     or die ("Ошибка подключения к базе данных " . mysqli_error()); 
 
-if (isset($_GET['del_by_id'])) {
-    $sql = 'DELETE FROM example_newsp WHERE id_example =' .$_GET['del_by_id'];
+echo "Точно удалить?</br>";
+echo "<form method='GET' action='delete.php'>
+	<p><input type='submit' name='yes' value='Да'></p>
+	<p><input type='submit' name='no' value='Нет'></p>
+	    </form>";
+if (isset($_GET['yes'])) {
+	$sql = 'DELETE FROM example_newsp WHERE id_example =' .$_GET['del_by_id'];
+    if (mysqli_query($link, $sql)) {
+    	echo "Удаление прошло успешно!<br>";
+		mysqli_close($link);
+	    echo '<a href="list.php">Нажми меня, чтобы вернуться к таблице!</a>';
+	} 
+	else {
+		echo "Что-то не так =(<br>" . mysqli_error($link);
+	}
 }
-if (mysqli_query($link, $sql)) {
-  echo "Удаление прошло успешно!<br>";
-  echo '<a href="list.php">Нажми меня, чтобы вернуться в таблице!</a>';
-} else {
-  echo "Что-то не так =(<br>" . mysqli_error($link);
+elseif (isset($_GET['no'])) {
+	echo 'No problem!';
+	echo '<a href="list.php">Нажми на меня, чтобы вернуться к таблице!</a>';
 }
 
-mysqli_close($link);
 ?>
