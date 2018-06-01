92ce56c6be877b8952febf1f4abd465fc8d536d5
Alina <dimova_alina@mail.ru>
2018-06-01 16:55:38 +0300

    Работает как надо

--- edit.php
+++ edit.php
@@ -1,16 +1,19 @@
-<?php
+﻿<?php
 require_once 'connection.php'; 
  
 $link = mysqli_connect($host, $user, $password, $database)
     or die ("Ошибка подключения к базе данных " . mysqli_error());
 
+
 if (isset($_GET['red_by_id'])) { 
+    $id = $_GET['red_by_id'];
 
     $sql0 = "SELECT *
             FROM example_newsp
             WHERE id_example=" . $_GET['red_by_id'] ;
     $result0 = mysqli_query($link, $sql0);
     $start = mysqli_fetch_array($result0);
+    $price = $start['price'];
 
     $sql1 = "SELECT DISTINCT tp_name
             FROM example_newsp";
@@ -24,50 +27,56 @@ if (isset($_GET['red_by_id'])) {
             FROM example_newsp";
     $result3 = mysqli_query($link, $sql3);
 
-    echo "<form method='GET' action='edit.php'>
-        <p>Выбрать типографию: <select name='tp_name'>
-        <option value='$tp_name'>" . $start['tp_name'] . "</option>";
+    echo "<form method='POST' action='edit.php'>
+        <p>Идентификатор изменяемого экземпляра: <select name='id'> 
+        <option>" . $id . "</option>
+        </select></p>";
+
+    echo  "<p>Выбрать типографию: <select name='tp_name'>
+        <option>" . $start['tp_name'] . "</option>";
 
     while($row = mysqli_fetch_array($result1))
         {
-        echo "<option value='$tp_name'>" . $row['tp_name'] . "</option>";
+        echo "<option>" . $row['tp_name'] . "</option>";
         } 
     echo "</select></p>
         <p>Выбрать номер почтового отделения: <select name='id_post'>
-        <option value='$id_post'>" . $start['id_post'] . "</option>";
+        <option>" . $start['id_post'] . "</option>";
     while($row = mysqli_fetch_array($result2))
         {
-        echo "<option value='$id_post'>" . $row['id_post'] . "</option>";
+        echo "<option>" . $row['id_post'] . "</option>";
         }
     echo "</select></p>
         <p>Изменить название газеты: <select name='np_name'> 
-        <option value='$np_name'>" . $start['np_name'] . "</option>";
+        <option>" . $start['np_name'] . "</option>";
     while($row = mysqli_fetch_array($result3))
         {
-        echo "<option value='$np_name'>" . $row['np_name'] . "</option>";
+        echo "<option>" . $row['np_name'] . "</option>";
         }
     echo "</select></p>
-            <p>Изменить цену: <input type='text' name='price' value='$price' size='20'> </p>
+            <p>Изменить цену: <input type='text' name='price' value= $price size='20'> </p>
     	    <p><input type='submit' name='enter' value='Сохранить'></p>
     	    </form>";
 
-    if (isset($_GET['enter'])) { 
-        $sql = mysql_query('UPDATE `example_newsp` SET '
-                .'`tp_name` = "'.$_GET['tp_name'].'",'
-                .'`id_post` = "'.$_GET['id_post'].'",'
-                .'`np_name` = "'.$_GET['np_name'].'",'
-                .'`price` = '.$_GET['price'].' '
-                .'WHERE `id_example` = '.$_GET['red_by_id']);
-        if (mysqli_query($link, $sql)) {
-            echo "Удаление прошло успешно!<br>";
-            mysqli_close($link);
-            echo '<a href="list.php">Нажми меня, чтобы вернуться в таблице!</a>';  
-        }
-        else{
-              echo "Кое-где ошибка<br>" . mysqli_error($link);
-        }
-    }
 }
 
+if (isset($_POST['enter'])) { 
+$sql = 'UPDATE example_newsp SET '
+    .'tp_name = "'.$_POST['tp_name'].'",'
+    .'id_post = "'.$_POST['id_post'].'",'
+    .'np_name = "'.$_POST['np_name'].'",'
+    .'price = '.$_POST['price'].' '
+    .'WHERE id_example = '.$_POST['id'];
+
+$result = mysqli_query($link, $sql);
+    if ($result) {
+    echo "Изменение записи прошло успешно!<br>";
+    mysqli_close($link);
+    echo '<a href="list.php">Нажми меня, чтобы вернуться в таблице!</a>';  
+    }
+    else{
+      echo "Кое-где ошибка<br>" . mysqli_error($link);
+    }
+}
 
 ?>
